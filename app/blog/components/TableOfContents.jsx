3ed8861a14ed2c474bc7b4da7c19e27475cc41c8
Freezeraid <kilanju@outlook.fr>
2025-02-26 11:12:01 +0100

    New article and fix table of content

--- app/blog/components/TableOfContents.jsx
+++ app/blog/components/TableOfContents.jsx
@@ -1,6 +1,5 @@
 'use client'
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
 
 export default function TableOfContents() {
  const [activeId, setActiveId] = useState('')
@@ -108,7 +107,7 @@ const scrollToHeading = (id) => {
            <div 
               onClick={() => heading.level === 'h2' ? scrollToHeading(heading.id) : null}
               className={`
-                w-full px-4 py-2 rounded-lg 
+                w-full pl-4 pr-6 py-2 rounded-lg 
                 transition-all duration-300 group group-hover:-translate-y-0.5
                 ${heading.level === 'h3' ? 'text-gray-500' : 
                   activeId === heading.id 
@@ -121,7 +120,7 @@ const scrollToHeading = (id) => {
                 {heading.text}
               </span>
               {activeId === heading.id && (
-                <span className="absolute right-3 top-1/2 -translate-y-1/2">
+                <span className="absolute right-1 top-1/2 -translate-y-1/2">
                   <svg 
                     className="w-5 h-5" 
                     fill="none" 
