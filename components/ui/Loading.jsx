dd368dfc34585ce0ad5d044a55b2978e0dbfbc76
Freezeraid <kilanju@outlook.fr>
2025-03-16 17:42:11 +0100

    Push better loading site

--- components/ui/Loading.jsx
+++ components/ui/Loading.jsx
@@ -1,8 +1,8 @@
 
 export default function Loading() {
-    return (
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600"></div>
-      </div>
-    )
-  }
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-white dark:bg-gray-900">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-600"></div>
+    </div>
+  )
+}
